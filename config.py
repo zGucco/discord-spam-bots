dc62dcc2d4459ff4cd5f39f508b812fe65e81fc5
zGucco <dylangodoyleonel@gmail.com>
2024-05-15 11:26:28 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # Basic Config (For all bots)
 pythonCommand = "python" # If you have the python command set to something else such as python3
-userToken = [''] # User Tokens here. example 'token','token' | 'email:pass', 'token'
-DiscordChannel = '' # Channel ID,
-SpamSpeed = 0.7 # Changes how fast the messages are posted. (Anything under 0.7 tends to break it (┛✧Д✧))┛彡┻━┻ )
+userToken = ['dylangodoyleonel588@gmail.com', 'dylanpro288'] # User Tokens here. example 'token','token' | 'email:pass', 'token'
+DiscordChannel = '965994870625931274' # Channel ID,
+SpamSpeed = 0.1 # Changes how fast the messages are posted. (Anything under 0.7 tends to break it (┛✧Д✧))┛彡┻━┻ )
 textRandom = False # if the root has 'text.txt' it will pick a random text line and send it.
 textFull = False # Will send everything written in 'text.txt'
 discumLog = False # Displays Discums Logs, Very useful for providing more info on what went wrong.
